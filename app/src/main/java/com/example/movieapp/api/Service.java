bcbbe384da18dcc01bfab988acd2452b23e23b25
ronak <ronakjilka@gmail.com>
2020-05-30 12:03:58 +0530

    Movie-app

---
+++ app/src/main/java/com/example/movieapp/api/Service.java
@@ -0,0 +1,30 @@
+package com.example.movieapp.api;
+
+import com.example.movieapp.model.MoviesResponse;
+import com.example.movieapp.model.Review;
+import com.example.movieapp.model.TrailerResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by delaroy on 5/18/17.
+ */
+public interface Service {
+
+    @GET("movie/popular")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{movie_id}/videos")
+    Call<TrailerResponse> getMovieTrailer(@Path("movie_id") int id, @Query("api_key") String apiKey);
+
+    //Reviews
+    @GET("movie/{movie_id}/reviews")
+    Call<Review> getReview(@Path("movie_id") int id, @Query("api_key") String apiKey);
+
+}
